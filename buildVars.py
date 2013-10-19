cdcd084925071eb62cc4b2a4780ad2e01325a343
Mesar Hameed <mhameed@src.gnome.org>
2013-10-19 12:22:32 +0200

    buildVars.py: include installTasks.py in i18nSources, so that the translatable messages are included in the pot file.

--- buildVars.py
+++ buildVars.py
@@ -35,7 +35,7 @@ import os.path
 pythonSources = [os.path.join("addon", "globalPlugins", "*.py")]
 
 # Files that contain strings for translation. Usually your python sources
-i18nSources = pythonSources + ["buildVars.py", "docHandler.py"]
+i18nSources = pythonSources + ["buildVars.py", "docHandler.py", os.path.join("addon", "installTasks.py")]
 
 # Files that will be ignored when building the nvda-addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
