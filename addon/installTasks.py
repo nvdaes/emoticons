31b01d47a21bd01b62c5afe83c1e1c1e6c834d65
Noelia Ruiz Martínez <nrm1977@gmail.com>
2013-10-25 06:35:03 +0200

    Request for importing dictionary if speechDicts/emoticons.dic is foun in installation.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -1,6 +1,9 @@
 # -*- coding: UTF-8 -*-
 
 import addonHandler
+import speechDictHandler
+import os
+import shutil
 import gui
 import wx
 
@@ -17,3 +20,14 @@ def onInstall():
 				wx.YES|wx.NO|wx.ICON_WARNING)==wx.YES:
 					addon.requestRemove()
 			break
+	if os.path.isfile(os.path.join(speechDictHandler.speechDictsPath, "emoticons.dic")):
+		if gui.messageBox(
+		# Translators: the label of a message box dialog.
+		_("You seem to have a dictionary for this add-on. Do you want to import it?"),
+		# Translators: the title of a message box dialog.
+		_("Import emoticons dictionary"),
+		wx.YES|wx.NO|wx.ICON_WARNING)==wx.YES:
+			try:
+				shutil.copy(os.path.join(speechDictHandler.speechDictsPath, "emoticons.dic"), os.path.join(os.path.dirname(__file__), "globalPlugins"))
+			except:
+				pass
