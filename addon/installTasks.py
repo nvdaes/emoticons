e6c506d1613011acdd66c50e7a982d921754069f
nvdaes <nrm1977@gmail.com>
2021-05-30 09:50:43 +0200

    Fix lint errors

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-#Copyright (C) 2013-2019 Noelia Ruiz Martínez, other contributors
+# Copyright (C) 2013-2019 Noelia Ruiz Martínez, other contributors
 # Released under GPL2
 
 import config
@@ -13,6 +13,7 @@ config.conf.spec["emoticons"] = confspec
 
 addonHandler.initTranslation()
 
+
 def onInstall():
 	import speechDictHandler
 	import globalVars
@@ -20,35 +21,47 @@ def onInstall():
 	import shutil
 	import gui
 	import wx
-	ADDON_DICTS_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "globalPlugins", "emoticons", "emoticons"))
+	ADDON_DICTS_PATH = os.path.abspath(
+		os.path.join(os.path.dirname(__file__), "globalPlugins", "emoticons", "emoticons")
+	)
 	EXPORT_DICTS_PATH = os.path.join(speechDictHandler.speechDictsPath, "emoticons")
-	if os.path.isfile(os.path.join(speechDictHandler.speechDictsPath, "emoticons.dic")) or os.path.isfile(os.path.join(globalVars.appArgs.configPath, "emoticons.ini")) or os.path.isdir(EXPORT_DICTS_PATH):
+	if (
+		os.path.isfile(os.path.join(speechDictHandler.speechDictsPath, "emoticons.dic"))
+		or os.path.isfile(os.path.join(globalVars.appArgs.configPath, "emoticons.ini"))
+		or os.path.isdir(EXPORT_DICTS_PATH)
+	):
 		if gui.messageBox(
-			# Translators: the label of a message box dialog.
-			_("You seem to have previous settings saved for this add-on. Do you want to import them, removing deprecated files?"),
+			_(
+				# Translators: the label of a message box dialog.
+				"You seem to have previous settings saved for this add-on."
+				" Do you want to import them, removing deprecated files?"
+			),
 			# Translators: the title of a message box dialog.
 			_("Import Emoticons add-on settings"),
-			wx.YES|wx.NO|wx.ICON_WARNING)==wx.YES:
+			wx.YES | wx.NO | wx.ICON_WARNING
+		) == wx.YES:
+			try:
+				os.makedirs(EXPORT_DICTS_PATH)
+				shutil.copy(os.path.join(speechDictHandler.speechDictsPath, "emoticons.dic"), EXPORT_DICTS_PATH)
+			except Exception:
+				pass
+			try:
+				shutil.copytree(EXPORT_DICTS_PATH, ADDON_DICTS_PATH)
+			except Exception:
+				pass
+			try:
+				os.remove(os.path.join(speechDictHandler.speechDictsPath, "emoticons.dic"))
+			except Exception:
+				pass
+			if os.path.isfile(os.path.join(globalVars.appArgs.configPath, "emoticons.ini")):  # Activate
+				config.conf["emoticons"]["announcement"] = 1
 				try:
-					os.makedirs(EXPORT_DICTS_PATH)
-					shutil.copy(os.path.join(speechDictHandler.speechDictsPath, "emoticons.dic"), EXPORT_DICTS_PATH)
-				except:
+					os.remove(os.path.join(globalVars.appArgs.configPath, "emoticons.ini"))
+				except Exception:
 					pass
-				try:
-					shutil.copytree(EXPORT_DICTS_PATH, ADDON_DICTS_PATH)
-				except:
-					pass
-				try:
-					os.remove(os.path.join(speechDictHandler.speechDictsPath, "emoticons.dic"))
-				except:
-					pass
-				if os.path.isfile(os.path.join(globalVars.appArgs.configPath, "emoticons.ini")): # We suppose that emoticons speaking should be activated
-					config.conf["emoticons"]["announcement"] = 1
-					try:
-						os.remove(os.path.join(globalVars.appArgs.configPath, "emoticons.ini"))
-					except:
-						pass
-				return
-	previousDictsPath = os.path.join(globalVars.appArgs.configPath, "addons", "emoticons", "globalPlugins", "emoticons", "emoticons")
+			return
+	previousDictsPath = os.path.join(
+		globalVars.appArgs.configPath, "addons", "emoticons", "globalPlugins", "emoticons", "emoticons"
+	)
 	if os.path.isdir(previousDictsPath):
 		shutil.copytree(previousDictsPath, ADDON_DICTS_PATH)
