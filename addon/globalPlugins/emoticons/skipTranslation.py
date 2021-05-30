c75b124d60f8c00bce87c07bd247a109dfcb980f
nvdaes <nrm1977@gmail.com>
2021-05-30 11:38:55 +0200

    Lint (#26)
    * Fix lint errors
    
    * Fix almost all lint errors, to ensure that GitHub workflow is right
    
    * Fix lint error
    
    * Fix comments for translators

--- addon/globalPlugins/emoticons/skipTranslation.py
+++ addon/globalPlugins/emoticons/skipTranslation.py
@@ -2,8 +2,9 @@
 # skipTranslation: Module to get translated texts from NVDA
 # Based on implementation made by Alberto Buffolino
 # https://github.com/nvaccess/nvda/issues/4652
-#Copyright (C) 2016 Noelia Ruiz Martínez
+# Copyright (C) 2016 Noelia Ruiz Martínez
 # Released under GPL 2
+
 
 def translate(text):
 	return _(text)
